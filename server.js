ea216ff8fcd62f2dd178ea78c1a9d55acacdc65a
Ali Hassan <56788298+1234-ali@users.noreply.github.com>
2021-05-20 04:31:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 db.sequelize.sync();
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
 
 
 app.get("/", (req, res) => {
